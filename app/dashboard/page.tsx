4f38ec5fbdd76173172b59ea89586d3e070a73a9
AmirAbbasPZK0 <amirabbaspezeshki2@gmail.com>
2024-11-01 18:16:36 +0330

    Counter

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 const Dashboard = () => {
     return (<>
         <div className="flex items-center justify-center h-[100vh]">
-            <h1>Dashboard</h1>
+           <div className="rounded-md border-2 border-slate-300 p-6">
+            <h1 className="text-[30px]">Dashboard</h1>
+           </div>
         </div>
     </>);
 }
